7f5baf353992ce0998ccfe037b2d8c06c2774fdc
Mickael <mickaelvinet79@gmail.com>
2021-02-01 12:56:15 +0100

    Fix duplicate items

--- syncro/__init__.py
+++ syncro/__init__.py
@@ -8,26 +8,25 @@ from bs4 import BeautifulSoup
 API = "https://api.spotify.com/v1"
 URL = "https://www.6play.fr/rtl2/quel-est-ce-titre"
 
-token = os.getenv("token")
-client_id = os.getenv("client_id")
-client_secret = os.getenv("client_secret")
+TOKEN = os.getenv("token")
+CLIENT_ID = os.getenv("client_id")
+CLIENT_SECRET = os.getenv("client_secret")
 
 print("Run with")
-print("Token        = " + token)
-print("ClientId     = " + client_id)
-print("ClientSecret = " + client_secret)
+print("Token        = " + TOKEN)
+print("ClientId     = " + CLIENT_ID)
+print("ClientSecret = " + CLIENT_SECRET)
 print(" ")
 
 
 def get_token():
     query = requests.post("https://accounts.spotify.com/api/token", {
-        "refresh_token": token,
+        "refresh_token": TOKEN,
         "grant_type": "refresh_token",
-        "client_id": client_id,
-        "client_secret": client_secret
-    }).json()
-
-    return query["access_token"]
+        "client_id": CLIENT_ID,
+        "client_secret": CLIENT_SECRET
+    })
+    return query.json()["access_token"]
 
 
 def get_last_music():
@@ -45,9 +44,9 @@ def get_last_music():
     return musics
 
 
-def get_track_id_spotify(music):
+def get_track_id_spotify(music, token):
     query = "q=" + music + "&type=track"
-    spotify = requests.get(API + "/search?" + query, headers={"Authorization": "Bearer " + get_token()})
+    spotify = requests.get(API + "/search?" + query, headers={"Authorization": "Bearer " + token})
 
     result = spotify.json()["tracks"]["items"]
     if len(result) > 0:
@@ -56,38 +55,43 @@ def get_track_id_spotify(music):
     return None
 
 
-def track_already_exist(track):
-    spotify = requests.get(API + "/playlists/6iazs8VECddcN2EtJFDhVA/tracks?market=FR&fields=items(track(uri))",
-                           headers={"Authorization": "Bearer " + get_token()})
+def track_already_exist(track, i, token):
+    request = "?market=FR&fields=items(track(uri)),total&offset=" + str(i)
+    spotify = requests.get(API + "/playlists/6iazs8VECddcN2EtJFDhVA/tracks" + request,
+                           headers={"Authorization": "Bearer " + token})
     exist = False
-
-    for item in spotify.json()["items"]:
+    result = spotify.json()
+    for item in result["items"]:
         if item["track"]["uri"] == track:
             exist = True
 
-    return exist
+    if result["total"] > i and exist is False:
+        return track_already_exist(track, i + 100, token)
+    else:
+        return exist
 
 
-def add_music_to_playlist(track):
+def add_music_to_playlist(track, token):
     spotify = requests.post(API + "/playlists/6iazs8VECddcN2EtJFDhVA/tracks?uris=" + track,
                             headers={
-                                "Authorization": "Bearer " + get_token()})
+                                "Authorization": "Bearer " + token})
     print(spotify.json())
 
 
 def run():
+    token = get_token()
     print("Run at " + datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     musics = get_last_music()
 
     for music in musics:
-        track_id = get_track_id_spotify(music)
+        track_id = get_track_id_spotify(music, token)
 
         if track_id is not None:
-            if not track_already_exist(track_id):
-                add_music_to_playlist(track_id)
+            if not track_already_exist(track_id, 0, token):
+                add_music_to_playlist(track_id, token)
         else:
             log = open("log.txt", "a")
-            log.write("Can't find the music " + music)
+            log.write("Can't find the music " + music + "\n")
 
     time.sleep(600)
     run()
